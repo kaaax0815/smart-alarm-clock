2e7b83dffe2935e0e93d8bcf92f312598a360c1e
kaaax0815 <999999bst@gmail.com>
2021-12-03 21:35:22 +0100

    Clean Working Directory, Better PackageJSON

--- scripts/_upload.sh
+++ scripts/_upload.sh
@@ -1,8 +1,13 @@
 set -e
 cd ../
+printf 'Cleaning Working Directory\n'
+ssh -i ~/.ssh/id_rsa pi@192.168.178.47 /bin/bash << EOF
+   rm -rf smart-alarm-clock/frontend
+   rm -rf smart-alarm-clock/backend
+EOF
 printf 'Uploading Frontend\n'
-rsync --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r frontend pi@192.168.178.47:/home/pi/smart-alarm-clock
+rsync --exclude={'node_modules/','dist'} --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r frontend pi@192.168.178.47:/home/pi/smart-alarm-clock
 printf 'Uploading Backend\n'
-rsync --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r backend pi@192.168.178.47:/home/pi/smart-alarm-clock
+rsync --exclude={'node_modules/','dist'} --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r backend pi@192.168.178.47:/home/pi/smart-alarm-clock
 echo 'Done! Enter to exit'
 read junk
