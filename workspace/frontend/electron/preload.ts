4e9650c608ccf75ced30ad88a4d0705ad962818f
kaaax0815 <999999bst@gmail.com>
2021-12-28 19:33:27 +0100

    Frontend: Pin execa to non esm version

--- workspace/frontend/electron/preload.ts
+++ workspace/frontend/electron/preload.ts
@@ -1,4 +1,4 @@
-import { execaCommandSync as execa } from 'execa';
+import { commandSync as execa } from 'execa';
 
 (window as Window & typeof globalThis & exec).exec = execa;
 export type exec = { exec: typeof execa };
