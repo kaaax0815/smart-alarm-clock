6c76f8ed346b9aed84bd6c85e2da826250fea59f
kaaax0815 <999999bst@gmail.com>
2022-02-28 17:58:08 +0100

    Frontend: correctly reference exec type

--- workspace/frontend/src/vite-env.d.ts
+++ workspace/frontend/src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
-export {};
+import { exec as Iexec } from '../electron/preload';
 
 declare global {
   interface Window {
-    exec: any;
+    exec: Iexec;
   }
   interface ImportMetaEnv {
     readonly VITE_BACKEND_PORT: string;
