b139ffc4e692df1db1090f9c99eaf1c4bd86940d
kaaax0815 <999999bst@gmail.com>
2022-03-20 20:12:19 +0100

    Backend: Add deleteRingtones route

---
+++ workspace/backend/src/Models/Controllers/deleteRingtones.ts
@@ -0,0 +1,7 @@
+export interface deleteRingtonesRequest {
+  ringtone: { name: string; location: string };
+}
+
+export interface deleteRingtonesResponse {
+  status: string;
+}
--- workspace/backend/src/Models/index.ts
+++ workspace/backend/src/Models/index.ts
@@ -2,5 +2,6 @@ export * from './Controllers/postSettings';
 export * from './Controllers/getSettings';
 export * from './Controllers/postRingtones';
 export * from './Controllers/getRingtones';
+export * from './Controllers/deleteRingtones';
 export * from './database';
 export * from './Express';
