6a0d8290e450b98ba4ef085abdc6530ddf3c0c7d
kaaax0815 <999999bst@gmail.com>
2022-03-23 15:43:40 +0100

    Backend: Dont allow functions to use push directly

--- workspace/backend/src/database.ts
+++ workspace/backend/src/database.ts
@@ -1,13 +1,14 @@
 import { JsonDB } from 'node-json-db';
 
 import { socketIO } from './index';
+import { database } from './Models/database';
 
 class CustomDB extends JsonDB {
   constructor(path: string) {
     super(path, true);
     this.initializeDatabase();
   }
-  initializeDatabase() {
+  private initializeDatabase() {
     try {
       if (this.getData('/initialized') === true) {
         return;
@@ -15,20 +16,43 @@ class CustomDB extends JsonDB {
     } catch {
       console.log('Database not initialized');
     }
-    this.push('/settings/timezone', 'Europe/Berlin');
-    this.push('/settings/location', {
+    this.setTimezone('Europe/Berlin');
+    this.setLocation({
       city: 'Berlin',
       countryCode: 'DE',
       lat: 52.5170365,
       lon: 13.3888599
     });
-    this.push('/ringtones', [{ name: 'Alarm', location: '/ringtones/Alarm.mp3' }]);
-    this.push('/initialized', true);
+    this.addRingtone({ name: 'Alarm', location: '/ringtones/Alarm.mp3' });
+    this._push('/initialized', true);
+  }
+  /**
+   * Does nothing
+   * Use typed methods to update database
+   */
+  push() {
+    return;
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  push(dataPath: string, data: any, override?: boolean): void {
+  private _push(dataPath: string, data: any, override?: boolean): void {
     super.push(dataPath, data, override);
     socketIO?.emitAll('databaseChange');
+  }
+  setTimezone(timezone: database['settings']['timezone']) {
+    this._push('/settings/timezone', timezone);
+  }
+  setLocation(location: database['settings']['location']) {
+    this._push('/settings/location', location);
+  }
+  addRingtone(ringtone: database['ringtones'][0]) {
+    this._push('/ringtones', [ringtone], true);
+  }
+  deleteRingtone(ringtone: database['ringtones'][0]) {
+    const oldRingtones = this.getData('/ringtones') as database['ringtones'];
+    const newRingtones = oldRingtones.filter(
+      (item) => item.name !== ringtone.name && item.location !== ringtone.location
+    );
+    this._push('/ringtones', newRingtones, false);
   }
 }
 
