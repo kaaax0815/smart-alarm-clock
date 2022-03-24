82b3664957f6a01f1bb7b5e822ded6b2e9bf0c91
kaaax0815 <999999bst@gmail.com>
2022-03-24 18:48:47 +0100

    Frontend: Get German Weather responses

--- workspace/frontend/src/hooks/useWeather.ts
+++ workspace/frontend/src/hooks/useWeather.ts
@@ -1,4 +1,4 @@
-import { Exclude, OpenWeatherMap, Units } from 'owm-onecall-api';
+import { Exclude, Language, OpenWeatherMap, Units } from 'owm-onecall-api';
 import { useQuery } from 'react-query';
 
 import useSettings from './useSettings';
@@ -15,6 +15,7 @@ function useWeather() {
       openWeather
         .builder(settingsData!.location.lat, settingsData!.location.lon)
         .exclude(Exclude.Minutely)
+        .language(Language.German)
         .execute(),
     {
       enabled: !!settingsData,
