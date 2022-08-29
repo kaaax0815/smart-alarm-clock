76aca499102daa6a35180bafc09060f9275277f0
kaaax0815 <999999bst@gmail.com>
2022-08-30 01:58:08 +0200

    frontend: add slight backdrop behind weather icons

--- workspace/frontend/src/screens/Clock/components/Weather.tsx
+++ workspace/frontend/src/screens/Clock/components/Weather.tsx
@@ -9,12 +9,14 @@ import styles from './Weather.module.css';
 function Weather(): JSX.Element {
   const { data: weatherData, status: weatherStatus } = useWeather();
   const navigate = useNavigate();
+
   if (weatherStatus !== 'success') {
     return <CircularProgress color="inherit" />;
   }
+
   return (
     <div className={styles.Weather} onClick={() => navigate('/weather')}>
-      {weatherData!.daily!.map((item) => (
+      {weatherData.daily!.map((item) => (
         <div className={styles.WeatherCard} key={item.dt}>
           <div className="__Weather-Card-Date">{relativeDaysFromUnix(item.dt)}</div>
           <WeatherIcon
