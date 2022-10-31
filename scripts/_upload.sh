12d2f7894c8860e4c5459cade6ebbfbc1ed083bd
kaaax0815 <999999bst@gmail.com>
2022-10-31 19:02:54 +0100

    scripts: ignore build artifact

--- scripts/_upload.sh
+++ scripts/_upload.sh
@@ -6,7 +6,7 @@ ssh -i ~/.ssh/id_rsa pi@192.168.178.47 /bin/bash << EOF
    rm -rf smart-alarm-clock/backend
 EOF
 printf 'Uploading Workspace\n'
-rsync --exclude={'node_modules/','dist'} --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r workspace/ pi@192.168.178.47:/home/pi/smart-alarm-clock
+rsync --exclude={'node_modules/','dist','build'} --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r workspace/ pi@192.168.178.47:/home/pi/smart-alarm-clock
 printf 'Uploading Start Script\n'
 rsync --info=progress2 -e "ssh -i ~/.ssh/id_rsa" -r scripts/smart-alarm-clock pi@192.168.178.47:/home/pi/smart-alarm-clock
 ssh -i ~/.ssh/id_rsa pi@192.168.178.47 /bin/bash << EOF
